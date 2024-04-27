45e9019b9fa2b0ca79c6b95276d02d5ada8bb97a
stanleytang <tangshunlei@gmail.com>
2024-04-28 00:35:07 +0800

    Add basic calculations

--- pipe-bender/src/PipeBender.tsx
+++ pipe-bender/src/PipeBender.tsx
@@ -1,17 +1,49 @@
 import React, { useState } from 'react';
 import './PipeBender.css';
+import { Vec3 } from 'vec3';
+
+interface RoundedBend
+{
+  center: Vec3;
+  start: Vec3;
+  end: Vec3;
+  centralAngle: number;
+  radius: number;
+}
+
+function calcRoundedBend(points: [Vec3, Vec3, Vec3], radius: number) {
+  const p0 = points[0];
+  const p1 = points[2];
+  const p = points[1];
+  
+  const v0 = p0.clone().subtract(p).normalize();
+  const v1 = p1.clone().subtract(p).normalize();
+  const h = v0.clone().add(v1).normalize();
+
+  const cosTheta = h.innerProduct(v0.clone().normalize());
+  const sinTheta = Math.sqrt(1.0 - cosTheta * cosTheta);
+  const t = radius / sinTheta;
+
+  const center = p.clone().add(h.clone().scale(t));
+  const centralAngle = Math.acos(cosTheta) * 2;
+  const start = p.clone().add(v0.clone().scale(cosTheta * t));
+  const end = p.clone().add(v1.clone().scale(cosTheta * t));
+
+  let result: RoundedBend = { center: center, start: start, end: end, centralAngle: centralAngle, radius: radius };
+  return result;
+}
 
 function PipeBender() {
-  const [points, setPoints] = useState<{ x: number; y: number; z: number }[]>([{ x:0, y:0, z:0 }]);
+  const [points, setPoints] = useState<Vec3[]>([new Vec3(0, 0, 0), new Vec3(0, 0, -900), new Vec3(300, 1000, -300), new Vec3(300, 2000, -300)]);
   const [parameters, setParameters] = useState<{ radius: number; degreeFormat: 'radius' | 'degree' }>({
-    radius: 0,
+    radius: 200,
     degreeFormat: 'radius',
   });
   const [tableData, setTableData] = useState<{ name: string; value: string }[]>(Array.from(Array(10), () => ({ name: '', value: '' })));
 
   const handlePointChange = (index: number, field: keyof typeof points[0], value: number) => {
     const updatedPoints = [...points];
-    updatedPoints[index][field] = value;
+    // updatedPoints[index][field] = value;
     setPoints(updatedPoints);
   };
 
@@ -25,8 +57,10 @@ function PipeBender() {
     setTableData(updatedTableData);
   };
 
-  const handleCalculate = () => {
+  const handleCalculate = (points: Vec3[], radius: number) => {
     // Perform calculation
+    const bend = calcRoundedBend([ points[0], points[1], points[2]], parameters.radius);
+    console.log(bend.centralAngle);
   };
 
   const handleSave = () => {
@@ -119,12 +153,12 @@ function PipeBender() {
           </select>
         </div>
         <h2>Operators</h2>
-        <button onClick={handleCalculate}>Calculate</button>
+        <button onClick={() => handleCalculate(points, parameters.radius)}>Calculate</button>
         <button onClick={handleSave}>Save</button>
       </div>
       <div className="panel">
         <h2>Execution</h2>
-        <button onClick={handleCalculate}>Calculate</button>
+        <button onClick={() => handleCalculate(points, parameters.radius)}>Calculate</button>
         <button onClick={handleSave}>Save</button>
       </div>
     </div>
